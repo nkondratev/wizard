5dbdb2fd9e491292f2700c82fff8f0726cd7e11d
nkondratev <nikita.kondratiev01@gmail.com>
2026-01-28 11:29:53 +0300

    add others architectures

--- wizard.sh
+++ wizard.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 ARCH=$(uname -m)
 
+declare -A ARCHS
+
+ARCHS["x86_64"]="amd64"
+ARCHS["i386"]="386"
+ARCHS["aarch64"]="arm64"
+ARCHS["armv6l"]="armv6l"
+
 CMD=$1
 version=$2
 
@@ -8,13 +15,18 @@ LIST="list"
 USE="use"
 INSTALL="install"
 
+if [[ -z "${ARCHS[$ARCH]}" ]]; then
+    echo "Unsupported architecture: $ARCH"
+    exit 1
+fi
+
 if [[ -z "$GOPATH" ]]; then
     echo 'GOPATH="$HOME/.wizard/go"' >> "$HOME/.bashrc"
     echo 'GOBIN="$HOME/.wizard/go/bin"' >> "$HOME/.bashrc"
     echo 'PATH="$PATH:$GOBIN"' >> "$HOME/.bashrc"
 fi
 
-if [ ! -d "~/.wizard" ]; then
+if [ ! -d "$HOME/.wizard" ]; then
     mkdir -p $HOME/.wizard/bin
     mkdir -p $HOME/.wizard/versions
 
@@ -33,8 +45,8 @@ EOF
 
 case $CMD in
     $INSTALL)
-    wget "https://go.dev/dl/go$version.linux-amd64.tar.gz"
-    tar -xf "go$version.linux-amd64.tar.gz" && rm -rf "go$version.linux-amd64.tar.gz"
+    wget "https://go.dev/dl/go$version.linux-${ARCHS[$ARCH]}.tar.gz"
+    tar -xf "go$version.linux-${ARCHS[$ARCH]}.tar.gz" && rm -rf "go$version.linux-${ARCHS[$ARCH]}.tar.gz"
     mv --force go $HOME/.wizard/versions/go$version
     ;;
     $USE)
@@ -42,6 +54,9 @@ case $CMD in
     ;;
     $LIST)
     printInfo
+    *)
+    printInfo
+    ;;
 
 esac
 
